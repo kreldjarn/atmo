8245993be3b8f38dba9ca710b4c3161baca372cb
Kristján Eldjárn <kristjan@eldjarn.net>
2017-05-17 20:32:54 +0000

    Created serializer and set up guardian

--- music_geocache/config/config.exs
+++ music_geocache/config/config.exs
@@ -22,6 +22,17 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+
+config :guardian, Guardian,
+  allowed_algos: ["HS512"],
+  verify_module: Guardian.JWT,
+  issuer: "MusicGeocache",
+  ttl: {30, :days},
+  allowed_drift: 2000,
+  verify_issuer: true,
+  secret_key: "shush_this_is_a_dev_secret", #TODO
+  serializer: MusicGeocache.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
