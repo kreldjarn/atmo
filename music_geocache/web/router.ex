74b95faa65c395d77727cc2da6191a340fa958d4
Kristján Eldjárn <kristjan@eldjarn.net>
2017-05-17 22:41:33 +0000

    Added serializer

--- music_geocache/web/router.ex
+++ music_geocache/web/router.ex
@@ -7,6 +7,14 @@ defmodule MusicGeocache.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
+  pipeline :browser do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.EnsureAuthenticated, handler: MusicGeocache.Token
+    plug Guardian.Plug.LoadResource
   end
 
   pipeline :api do
